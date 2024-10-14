4bda5921c447b2e4c6e2fee4961feb1f72018f88
Bhargav Choppalli <bhargavchoppalli1998@gmail.com>
2024-10-14 14:48:14 +0530

    added basename

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import{ HashRouter as Router, Route, Routes } from 'react-router-dom'
+import{ Route, Routes, BrowserRouter } from 'react-router-dom'
 import Home from './components/Home'
 import Contact from './components/Contact'
 import Services from './components/Services'
@@ -8,14 +8,14 @@ import About from './components/About'
 
 const App = () => {
   return (
-    <Router>
+    <BrowserRouter basename='/React-routing'>
       <Routes>
         <Route path="/" element={<Home/>}/>
         <Route path="about" element={<About/>}/>
         <Route path="services" element={<Services/>}/>
         <Route path="contact" element={<Contact/>}/>
       </Routes>
-    </Router>
+    </BrowserRouter>
     
   )
 }
